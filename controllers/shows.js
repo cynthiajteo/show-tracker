49ac7b1ad063eaf2060b34d2cb9adc07be18f5a6
cynthiajteo <cynthiajteo@gmail.com>
2021-02-20 17:09:26 +0800

    setup app routes

--- controllers/shows.js
+++ controllers/shows.js
@@ -1,18 +1,67 @@
 const express = require('express');
-const router = express.Router();
+const shows = express.Router();
 const Show = require('../models/users.js');
 
 // normal routes will be in this file
-router.get('/', (req, res) => {
-    res.render('app/index.ejs');
-});
 
-router.get('/edit', (req, res) => {
-    res.render('app/edit.ejs');
-});
-
-router.get('/new', (req, res) => {
+// new route
+shows.get('/new', (req, res) => {
     res.render('app/new.ejs');
 });
 
-module.exports = router;
+// create route
+shows.post('/', (req, res) => {
+    Show.create(req.body, (error, createdProduct) => {
+        // res.send(createdProduct);
+        res.redirect('/app');
+    });
+});
+
+// index route
+shows.get('/', (req, res) => {
+    Show.find({}, (error, allShows) => {
+        res.render('index.ejs', {
+            shows: allShows,
+        });
+    });
+});
+
+// show route
+shows.get('/:id', (req, res) => {
+    Show.findById(req.params.id, (error, foundShow) => {
+        res.render('show.ejs', {
+            show: foundShow,
+        });
+    });
+});
+
+// delete route
+shows.delete('/:id', (req, res) => {
+    Show.findByIdAndRemove(req.params.id, (error, data) => {
+        res.redirect('/app');
+    });
+});
+
+// edit route
+shows.get('/:id/edit', (req, res) => {
+    Show.findById(req.params.id, (error, foundShow) => {
+        res.render('edit.ejs', {
+            show: foundShow,
+        });
+    });
+});
+
+// put edit
+shows.put('/:id', (req, res) => {
+    const index = req.params.id;
+    Show.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true },
+        (err, updatedProduct) => {
+            res.redirect(`/app/${index}`);
+        },
+    );
+});
+
+module.exports = shows;
