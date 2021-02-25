3cb4a883cd4c689546beb12ab13341420abfde6e
cynthiajteo <cynthiajteo@gmail.com>
2021-02-25 22:46:11 +0800

    delete route working

--- controllers/app.js
+++ controllers/app.js
@@ -55,25 +55,44 @@ router.get('/', isAuthenticated, (req, res) => {
 
 // show route
 router.get('/:id', (req, res) => {
-    console.log(req.params.id);
+    //console.log(req.session.currentUser._id);
+    //console.log(req.params._id);
     User.find(
-        { shows: Mongoose.Types.ObjectId(req.params.id) },
-        (err, foundShow) => {
-            // res.render('app/show.ejs');
-            console.log(foundShow);
+        { _id: req.session.currentUser._id },
+        {
+            shows: {
+                $elemMatch: {
+                    _id: req.params.id,
+                },
+            },
+        },
+        {
+            'shows.$': 1,
+        },
+        function (err, results) {
+            console.log(results[0].shows[0]);
+            res.render('app/show.ejs', {
+                show: results[0].shows[0],
+            });
         },
     );
 });
 
 // delete route
-// router.delete('/:id', (req, res) => {
-//     User.findByIdAndRemove(
-//         { _id: req.session.currentUser.shows._id },
-//         (error, data) => {
-//             res.redirect('/app');
-//         },
-//     );
-// });
+router.delete('/:id', (req, res) => {
+    User.findByIdAndUpdate(
+        { _id: req.session.currentUser._id },
+        { $pull: { shows: { _id: req.params.id } } },
+        { new: true },
+        function (error, model) {
+            if (error) {
+                return res.json(error);
+            } else {
+                return res.redirect('/app');
+            }
+        },
+    );
+});
 
 // edit route
 // router.get('/:id/edit', (req, res) => {
